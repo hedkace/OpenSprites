be6210fa9e8a2107a553680f9863aa2a416aeca5
Liam (Aqua) <liam4@users.noreply.github.com>
2015-05-14 08:15:25 -0300

    Update visualiser-base.js

--- gh/visualisers/visualiser-base.js
+++ gh/visualisers/visualiser-base.js
@@ -19,33 +19,35 @@ function visualizer(audio, fftSize, f) {
         frame: f
     };
     
-    // Load temporary saved data
-    // required if in a scope, e.g.
-    // window.onload
-    var audio = document.querySelector(VISUALIZER_TEMP_DATA.audio);
-    var fftSize = VISUALIZER_TEMP_DATA.fftSize;
-    var f = VISUALIZER_TEMP_DATA.frame;
-    delete VISUALIZER_TEMP_DATA;
-
-    var ctx = new AudioContext();
-    analyser = ctx.createAnalyser();
-    analyser.fftSize = fftSize;
-    var audioSrc = ctx.createMediaElementSource(audio);
-    audioSrc.connect(analyser);
-    audioSrc.connect(ctx.destination);
-
-    var frequencyData = new Uint8Array(128); // analyser.frequencyBinCount
-
-    var renderFrame = function() {
-        requestAnimationFrame(renderFrame);
-
-        // Update data in frequencyData
-        analyser.getByteFrequencyData(frequencyData);
-
-        f(frequencyData);
-    }
-
-    audio.play();
-
-    renderFrame();
+    document.querySelector(audio).addEventListener('canplay', function() {
+        // Load temporary saved data
+        // required if in a scope, e.g.
+        // window.onload
+        var audio = document.querySelector(VISUALIZER_TEMP_DATA.audio);
+        var fftSize = VISUALIZER_TEMP_DATA.fftSize;
+        var f = VISUALIZER_TEMP_DATA.frame;
+        delete VISUALIZER_TEMP_DATA;
+    
+        var ctx = new AudioContext();
+        analyser = ctx.createAnalyser();
+        analyser.fftSize = fftSize;
+        var audioSrc = ctx.createMediaElementSource(audio);
+        audioSrc.connect(analyser);
+        audioSrc.connect(ctx.destination);
+    
+        var frequencyData = new Uint8Array(128); // analyser.frequencyBinCount
+    
+        var renderFrame = function() {
+            requestAnimationFrame(renderFrame);
+    
+            // Update data in frequencyData
+            analyser.getByteFrequencyData(frequencyData);
+    
+            f(frequencyData);
+        }
+    
+        audio.play();
+    
+        renderFrame();
+    });
 };
