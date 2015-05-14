5a9341bd362493774592a35c8f8c2d6385df65e9
Liam (Aqua) <liam4@users.noreply.github.com>
2015-05-14 08:10:17 -0300

    Update visualiser-base.js

--- gh/visualisers/visualiser-base.js
+++ gh/visualisers/visualiser-base.js
@@ -18,33 +18,34 @@ function visualizer(audio, fftSize, f) {
         fftSize: 256, //fftSize,
         frame: f
     };
-    //window.onload = function() {
-        // Load temporary saved data
-        var audio = document.querySelector(VISUALIZER_TEMP_DATA.audio);
-        var fftSize = VISUALIZER_TEMP_DATA.fftSize;
-        var f = VISUALIZER_TEMP_DATA.frame;
-        delete VISUALIZER_TEMP_DATA;
+    
+    // Load temporary saved data
+    // required if in a scope, e.g.
+    // window.onload
+    var audio = document.querySelector(VISUALIZER_TEMP_DATA.audio);
+    var fftSize = VISUALIZER_TEMP_DATA.fftSize;
+    var f = VISUALIZER_TEMP_DATA.frame;
+    delete VISUALIZER_TEMP_DATA;
 
-        var ctx = new AudioContext();
-        var audioSrc = ctx.createMediaElementSource(audio);
-        var analyser = ctx.createAnalyser();
-        audioSrc.connect(analyser);
-        audioSrc.connect(ctx.destination);
-        analyser.fftSize = fftSize;
+    var ctx = new AudioContext();
+    analyser = ctx.createAnalyser();
+    analyser.fftSize = fftSize;
+    var audioSrc = ctx.createMediaElementSource(audio);
+    audioSrc.connect(analyser);
+    audioSrc.connect(ctx.destination);
 
-        var frequencyData = new Uint8Array(128); // analyser.frequencyBinCount
+    var frequencyData = new Uint8Array(128); // analyser.frequencyBinCount
 
-        var renderFrame = function() {
-            requestAnimationFrame(renderFrame);
+    var renderFrame = function() {
+        requestAnimationFrame(renderFrame);
 
-            // Update data in frequencyData
-            analyser.getByteFrequencyData(frequencyData);
+        // Update data in frequencyData
+        analyser.getByteFrequencyData(frequencyData);
 
-            f(frequencyData);
-        }
+        f(frequencyData);
+    }
 
-        audio.play();
+    audio.play();
 
-        renderFrame();
-    //};
+    renderFrame();
 };
